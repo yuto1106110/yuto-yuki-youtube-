bf80a3c13f2b569bc375ac78384c5070729703ca
yuto_110 <yuto.oio5.2011@gmail.com>
2025-03-22 12:43:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -609,6 +609,9 @@ def list_page(response: Response, request: Request):
     return template("api-tokutei.html", {"request": request})
 
 
+@app.get("/drive", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("drive.html", {"request": request})
 @app.get("/othello", response_class=HTMLResponse)
 def list_page(response: Response, request: Request):
     return template("othello.html", {"request": request})
