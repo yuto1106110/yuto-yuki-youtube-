23f5e8fe05c225a1498866ff8d9e83255cf9ad07
yuto_110 <yuto.oio5.2011@gmail.com>
2025-03-25 10:30:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -629,6 +629,9 @@ def list_page(response: Response, request: Request):
 @app.get("/chatwork", response_class=HTMLResponse)
 def list_page(response: Response, request: Request):
     return template("chatwork.html", {"request": request})
+@app.get("/sand-chat", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("sand-chat.html", {"request": request})
 
 
 @app.get("/interland", response_class=HTMLResponse)
