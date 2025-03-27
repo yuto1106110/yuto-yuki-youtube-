9f7500efce5c7c6dc7295ca407cd26716ae603b3
yuto_110 <yuto.oio5.2011@gmail.com>
2025-03-27 18:16:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -635,6 +635,9 @@ def list_page(response: Response, request: Request):
 @app.get("/news", response_class=HTMLResponse)
 def list_page(response: Response, request: Request):
     return template("news.html", {"request": request})
+@app.get("/setting", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("setting.html", {"request": request})
 
 
 @app.get("/interland", response_class=HTMLResponse)
