f53747ce3d13a1c47379c8430acaec29f1698945
yuto_110 <yuto.oio5.2011@gmail.com>
2025-04-05 08:40:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -875,6 +875,9 @@ def list_page(response: Response, request: Request):
 @app.get("/1v1-lol", response_class=HTMLResponse)
 def list_page(response: Response, request: Request):
     return template("1v1-lol.html", {"request": request})
+@app.get("/paper", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("paper.html.html", {"request": request})
 
 
 
