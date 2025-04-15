2b7ef49c4cff13a144bc710b56bf744034395702
yuto_110 <yuto.oio5.2011@gmail.com>
2025-04-15 18:54:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -87,29 +87,11 @@ def gettingVideoData(videoid):
     ]
 
 
-def getRandomUserAgent():
-  user_agent = user_agents[random.randint(0, len(user_agents) - 1)]
-  print(user_agent)
-  return {
-    'User-Agent': user_agent
-  }
-
 class InvidiousAPI:
-def __init__(self, request = None):
-if request and hasattr(request, 'cookies'):
-source_url = request.cookies.get("api_source")
-        else:
-source_url = None
-
-if not source_url:
-  source_url = os.environ.get(
-    "DEFAULT_API_SOURCE",
-    "https://raw.githubusercontent.com/yuto1106110/yuto-yuki-youtube-1/main/APItati"
-  )
-
-self.all = ast.literal_eval(
-  requests.get(source_url, headers = getRandomUserAgent(), timeout = (1.0, 0.5)).text
-)
+def __init__(self):
+self.all =
+ast.literal_eval(requests.get('https://raw.githubusercontent.com/yuto1106110/yuto-yuki-youtube-1/main/APItati',
+headers=getRandomUserAgent(), timeout=(1.0, 0.5)).text)
 
 self.video = self.all['video']
 self.playlist = self.all['playlist']
@@ -121,9 +103,11 @@ self.check_video = False
 
 def info(self):
 return {
-  'API': self.all,
-  'checkVideo': self.check_video
+'API': self.all,
+'checkVideo': self.check_video
 }
+
+invidious_api = InvidiousAPI()
 
         
 
