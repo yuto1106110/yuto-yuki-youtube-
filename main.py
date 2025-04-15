54c1a2ae1bb48dee65a2ceb989313705686c9f2b
yuto_110 <yuto.oio5.2011@gmail.com>
2025-04-15 20:31:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -95,22 +95,9 @@ def getRandomUserAgent():
   }
 
 class InvidiousAPI:
-    def __init__(self, request=None):
-        if request and hasattr(request, 'cookies'):
-            source_url = request.cookies.get("api_source")
-        else:
-            source_url = None
-
-        if not source_url:
-            source_url = os.environ.get(
-                "DEFAULT_API_SOURCE",
-                "https://raw.githubusercontent.com/yuto1106110/yuto-yuki-youtube-1/main/APItati"
-            )
-
-        self.all = ast.literal_eval(
-            requests.get(source_url, headers=getRandomUserAgent(), timeout=(1.0, 0.5)).text
-        )
-
+    def __init__(self):
+        self.all = ast.literal_eval(requests.get('https://raw.githubusercontent.com/yuto1106110/yuto-yuki-youtube-1/main/APItati', headers=getRandomUserAgent(), timeout=(1.0, 0.5)).text)
+      
         self.video = self.all['video']
         self.playlist = self.all['playlist']
         self.search = self.all['search']
@@ -124,10 +111,9 @@ class InvidiousAPI:
             'API': self.all,
             'checkVideo': self.check_video
         }
-from fastapi import Request
-async def video(request: Request):
-    invidious_api = InvidiousAPI(request)
-    # 残りの処理
+
+        
+invidious_api = InvidiousAPI()
 
 url = requests.get('https://raw.githubusercontent.com/LunaKamituki/Yuki-BBS-Server-URL/refs/heads/main/server.txt', headers=getRandomUserAgent()).text.rstrip()
 
