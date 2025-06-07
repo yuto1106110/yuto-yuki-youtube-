a5cfb27979f62f855b6514a6686de22a8c84ccc9
yuto_110 <yuto.oio5.2011@gmail.com>
2025-06-07 18:03:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1026,6 +1026,13 @@ def list_page(response: Response, request: Request):
 @app.get("/othello", response_class=HTMLResponse)
 def list_page(response: Response, request: Request):
     return template("othello.html", {"request": request})
+@app.get("/among", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("among.html", {"request": request})
+@app.get("/among-1", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("among-1.html", {"request": request})
+
 @app.exception_handler(500)
 def error500(request: Request, __):
     return template("error.html", {"request": request, "context": '500 Internal Server Error'}, status_code=500)
